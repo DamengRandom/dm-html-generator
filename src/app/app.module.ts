deec14b72131c0bdb778def1245362835642a467
Damon Wu <damonwu0605@gmail.com>
2018-08-02 00:00:56 +1000

    dmhg/task03: create full screen menu and relative stylings setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,43 +4,60 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { DisqusModule } from 'ngx-disqus';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatButtonModule, MatCheckboxModule, MatIconModule, MatMenuModule, MatToolbarModule } from '@angular/material';
+import { MatButtonModule,
+  MatCheckboxModule,
+  MatDialogModule,
+  MatIconModule,
+  MatListModule,
+  MatMenuModule,
+  MatToolbarModule } from '@angular/material';
 import { NgModule } from '@angular/core';
 
 // components
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { FooterComponent } from './shared/components/footer/footer.component';
-import { HeaderComponent } from './shared/components/header/header.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { IntroPanelComponent } from './pages/dashboard/intro-panel/intro-panel.component';
 import { BackButtonComponent } from './shared/components/back-button/back-button.component';
-import { GeneratorComponent } from './pages/generator/generator.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { DisqusPanelComponent } from './pages/dashboard/disqus-panel/disqus-panel.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { GeneratorComponent } from './pages/generator/generator.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { IntroPanelComponent } from './pages/dashboard/intro-panel/intro-panel.component';
+import { MenuDialogComponent } from './shared/components/header/menu-dialog/menu-dialog.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
-    FooterComponent,
-    HeaderComponent,
-    NotFoundComponent,
-    IntroPanelComponent,
     BackButtonComponent,
-    GeneratorComponent,
+    DashboardComponent,
     DisqusPanelComponent,
+    FooterComponent,
+    GeneratorComponent,
+    HeaderComponent,
+    IntroPanelComponent,
+    MenuDialogComponent,
+    NotFoundComponent,
+    AboutComponent,
   ],
   imports: [
     AppRoutingModule,
     BrowserAnimationsModule,
     BrowserModule,
-    DisqusModule.forRoot('dmtips'),
+    DisqusModule.forRoot('dm-html-generator'),
     FlexLayoutModule,
     MatButtonModule,
     MatCheckboxModule,
+    MatDialogModule,
     MatIconModule,
+    MatListModule,
     MatMenuModule,
     MatToolbarModule,
+  ],
+  exports: [
+  ],
+  entryComponents: [
+    MenuDialogComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/routes/app-routing.module.ts
+++ src/app/shared/routes/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 // components
+import { AboutComponent } from '../../pages/about/about.component';
 import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
 import { GeneratorComponent } from '../../pages/generator/generator.component';
 import { NotFoundComponent } from '../../pages/not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
+  { path: 'about', component: AboutComponent },
   { path: 'generator', component: GeneratorComponent },
   { path: '404', component: NotFoundComponent },
   { path: '**', pathMatch: 'full', redirectTo: '404' }
