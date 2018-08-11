6519e2fdfab22f10d2b03fd3a97c7a2faa9e1240
Damon Wu <damonwu0605@gmail.com>
2018-08-11 21:53:55 +1000

    dmhg/task05: add preview functionality for the editor

--- src/app/pages/generator/adding-text/adding-text.component.ts
+++ src/app/pages/generator/adding-text/adding-text.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { ContentsPreviewComponent } from '../contents-preview/contents-preview.component';
 
 @Component({
   selector: 'app-adding-text',
@@ -8,18 +10,41 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 
 export class AddingTextComponent implements OnInit {
-  constructor() {}
+  htmlContent;
+  constructor(public dialog: MatDialog) {}
   ngOnInit() {}
 
   addText() {
-    const textDivNode = document.createElement('div');
-    textDivNode.classList.add('text-div-node');
-    document.getElementById('texter').setAttribute('contenteditable', 'true');
-    document.getElementById('texter').appendChild(textDivNode);
+    const divNode = document.createElement('div');
+    const lengthId = document.getElementById('paper').childNodes.length;
+    divNode.classList.add('text-div-node');
+    divNode.setAttribute('id', `divNode${lengthId}`);
+    divNode.setAttribute('contenteditable', 'true');
+    divNode.setAttribute('placeholder', 'Start typing ..');
+    setTimeout(() => {
+      document.getElementById(`divNode${lengthId}`).focus();
+    }, 0);
+    document.getElementById('paper').appendChild(divNode);
   }
 
   clearText() {
-    document.getElementById('texter').remove();
-    document.getElementById('texter').setAttribute('contenteditable', 'false');
+    const nodeTexter = document.getElementById('paper');
+    while (nodeTexter.hasChildNodes()) {
+      nodeTexter.removeChild(nodeTexter.lastChild);
+    }
+    document.getElementById('paper').setAttribute('contenteditable', 'false');
+  }
+
+  openPreview() {
+    this.htmlContent = document.getElementById('paper').innerHTML;
+    const dialogRef = this.dialog.open(ContentsPreviewComponent, {
+      width: '100vh',
+      height: '100vh',
+      data: { htmlContent: this.htmlContent }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.htmlContent = result;
+    });
   }
 }
